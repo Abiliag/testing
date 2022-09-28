3fba047417cfe6ad93f1a5596fe0ca2eba176450
Abiliag <89725773+Abiliag@users.noreply.github.com>
2022-09-28 12:29:46 +0800

    change empapp

--- EmpApp.py
+++ EmpApp.py
@@ -53,12 +53,11 @@ def attendance_input():
 
         if emp_id =='' or date =='' or time =='' or status =='':
             errorMsg = "Please fill in all the fields"
-            buttonMsg = "HELLO"
+            buttonMsg = "Fields is NULL"
             action = "/attendance/"
             return render_template('error-message.html',errorMsg=errorMsg,buttonMsg=buttonMsg,action=action)
 
         
-
         try:
              cursor.execute(insert_sql, (emp_id, date, time, status))
              db_conn.commit()
@@ -72,54 +71,9 @@ def attendance_input():
         return render_template('attendance-output.html', emp_id=emp_id, date=date, time=time, status=status)
 
 
-# @app.route("/addattendance", methods=['POST'])
-# def AddAttendance():
-#     emp_id = request.form['emp_id']
-#     date = request.form['first_name']
-#     time = request.form['last_name']
-#     status = request.form['pri_skill']
-   
-
-#     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-#     cursor = db_conn.cursor()
-
-#     if emp_image_file.filename == "":
-#         return "Please select a file"
-
-#     try:
-
-#         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-#         db_conn.commit()
-#         emp_name = "" + first_name + " " + last_name
-#         # Uplaod image file in S3 #
-#         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-#         s3 = boto3.resource('s3')
-
-#         try:
-#             print("Data inserted in MySQL RDS... uploading image to S3...")
-#             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-#             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-#             s3_location = (bucket_location['LocationConstraint'])
-
-#             if s3_location is None:
-#                 s3_location = ''
-#             else:
-#                 s3_location = '-' + s3_location
-
-#             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-#                 s3_location,
-#                 custombucket,
-#                 emp_image_file_name_in_s3)
-
-#         except Exception as e:
-#             return str(e)
-
-#     finally:
-#         cursor.close()
-
-#     print("all modification done...")
-#     return render_template('AddEmpOutput.html', name=emp_name)
-
+@app.route("/attendance/view", methods=['GET','POST'])
+def attendance_viewAll():
+    return render_template('attendance-view.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
